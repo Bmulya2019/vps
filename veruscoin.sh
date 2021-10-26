e1559bda6d6017f7c2fe925fc24df9bb6685562d
Bmulya2019 <93226621+Bmulya2019@users.noreply.github.com>
2021-10-27 04:53:06 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u walletkalian.workename_bebas_saja -p x --cpu 2
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
+tar xf lolMiner_v1.31_Lin64.tar.gz
+./lolMiner --algo ETCHASH --pool etc.2miners.com:1010 --user 0xf42d143bef0d5bf91bf7307e3a2e3384d0249ee6.Lokahurip
 while [ 1 ]; do
 sleep 3
 done
-sleep 999
+sleep 9999999
